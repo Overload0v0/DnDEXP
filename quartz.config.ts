12d40aac47dcbe40e3104393f1cae7e2f869b271
Overload0v0 <164251266+Overload0v0@users.noreply.github.com>
2024-03-22 03:09:02 -0500

    Quartz sync: Mar 22, 2024, 3:09 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🐉 Behind The DM's Screen v.01.1",
+    pageTitle: "🐉 Behind The DM's Screen v.01.2",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
